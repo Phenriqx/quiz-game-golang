b4bedec98d69f17bb96152796b51e9b4d7ac6557
Pedro Henrique <pedroxlbd@gmail.com>
2025-03-05 00:55:41 -0300

    added timer via goroutines

--- main.go
+++ main.go
@@ -6,11 +6,33 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func (g *GameState) RunGame() {
+	fmt.Println("You have 18 questions to answer. You have 1:30 minutes to complete the quiz.")
+	done := make(chan bool)
+
+	// Starts a 90-second countdown in a separate Goroutine.
+	go func() {
+		countdown := 15
+		for countdown > 0 {
+			time.Sleep(1 * time.Second)
+			countdown--
+			fmt.Printf("\033[31m%d seconds remaining\033[31m\n", countdown)
+		}
+		done <- true
+	} ()
+
 	// Print the questions and alternatives
 	for i, question := range g.Questions {
+		select {
+		case <-done:
+			fmt.Println("\n⏳ Time's up! Quiz over.")
+			return
+		default:
+		}
+		
 		fmt.Printf("\033[33m %d - %s \033[33m\n", i+1, question.Text)
 
 		for j, alternative := range question.Alternatives {
@@ -23,25 +45,38 @@ func (g *GameState) RunGame() {
 		var err error
 
 		for {
-			reader := bufio.NewReader(os.Stdin)
-			read, _ := reader.ReadString('\n')
+			select {
+			case <-done: // If time runs out, stop the quiz
+				fmt.Println("\n⏳ Time's up! Quiz over.")
+				return
+			default:
+				// Reading user input
+				reader := bufio.NewReader(os.Stdin)
+				read, _ := reader.ReadString('\n')
 
-			answer, err = toInt(read[:len(read)-1])
-			if err != nil {
-                fmt.Println(err)
-                continue
-            }
+				answer, err = toInt(read[:len(read)-1])
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+			}
 			break
 		}
-
-		if question.Answer == answer {
-			fmt.Println("Correct!")
-			g.Points += 10
-			fmt.Printf("Your current score is: %d\n", g.Points)
-		} else {
-			fmt.Println("Wrong!")
-			fmt.Printf("Your current score is: %d\n", g.Points)
-			fmt.Println("------------------------------")
+		
+		select {
+			case <-done:
+                fmt.Println("\n��� Time's up! Quiz over.")
+                return
+            default:
+                if question.Answer == answer {
+					fmt.Println("Correct!")
+					g.Points += 10
+					fmt.Printf("Your current score is: %d\n", g.Points)
+				} else {
+					fmt.Println("Wrong!")
+					fmt.Printf("Your current score is: %d\n", g.Points)
+					fmt.Println("------------------------------")
+				}
 		}
 	}
 }
